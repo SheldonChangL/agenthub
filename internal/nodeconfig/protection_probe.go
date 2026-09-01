83676e985ab5cfe8d807916563dd38790c66c82e
SheldonChang <sheldon.chang@jet-opto.com.tw>
2026-09-01 16:02:39 +0800

    test: probe whether branch protection blocks a failing merge
    Deliberately unparseable. Deleted as soon as the answer is known.
    
    Change-Id: I523d3326b14ba198a925d5053aef712ff3a5d8bb

---
+++ internal/nodeconfig/protection_probe.go
@@ -0,0 +1,5 @@
+package nodeconfig
+
+// This file exists only to make CI fail, verifying whether branch protection
+// actually blocks a merge on this plan. It is deleted immediately after.
+func deliberatelyBroken( {
